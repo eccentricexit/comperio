e7106e556e69b7419fad798baec59a62314e0814
Matheus Faria de Alencar <mtsalenc@gmail.com>
2017-11-26 09:34:50 -0200

    feat: Swap textview for spinner

--- app/src/main/java/com/rigel/comperio/view/FiltersFragment.java
+++ app/src/main/java/com/rigel/comperio/view/FiltersFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
+import com.rigel.comperio.adapters.SubjectAdapter;
 import com.rigel.comperio.databinding.FragmentFiltersBinding;
+import com.rigel.comperio.model.Subject;
 import com.rigel.comperio.viewmodel.FiltersViewModel;
 
 public class FiltersFragment extends BaseFragment {
@@ -33,7 +36,52 @@ public class FiltersFragment extends BaseFragment {
         fragmentFiltersBinding = FragmentFiltersBinding.inflate(inflater, container, false);
         fragmentFiltersBinding.setFiltersViewModel(filtersViewModel);
 
+        initSubjectSpinner();
+
         return fragmentFiltersBinding.getRoot();
     }
 
+    private void initSubjectSpinner() {
+        SubjectAdapter adapter = new SubjectAdapter(
+                getContext(),
+                android.R.layout.simple_spinner_item,
+                fragmentFiltersBinding.getFiltersViewModel().subjects
+        );
+
+        fragmentFiltersBinding.setSpinnerAdapter(adapter);
+
+        int selection = -1;
+
+        for(int i=0;i<fragmentFiltersBinding.getSpinnerAdapter().getCount();i++){
+            Subject subject = (Subject) fragmentFiltersBinding.getSpinnerAdapter().getItem(i);
+            if(subject.equals(filtersViewModel.filter.subject)){
+                selection = i;
+                break;
+            }
+        }
+
+        if(selection!=-1) {
+            fragmentFiltersBinding.subjectSpinner.setSelection(selection);
+        }
+
+        fragmentFiltersBinding.subjectSpinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        fragmentFiltersBinding.getFiltersViewModel().filter.subject =
+                                ((Subject) fragmentFiltersBinding.subjectSpinner.getSelectedItem()).name;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        //no op
+                    }
+                });
+    }
+
+    @Override
+    public void onStop() {
+        filtersViewModel.save(null);
+        super.onStop();
+    }
 }
--- app/src/main/java/com/rigel/comperio/viewmodel/FiltersViewModel.java
+++ app/src/main/java/com/rigel/comperio/viewmodel/FiltersViewModel.java
@@ -6,21 +6,25 @@ import com.rigel.comperio.DevUtils;
 import com.rigel.comperio.Navigator;
 import com.rigel.comperio.PersistenceManager;
 import com.rigel.comperio.model.Filter;
+import com.rigel.comperio.model.Subject;
 
 public class FiltersViewModel extends BaseViewModel {
 
     public Filter filter;
+    public Subject[] subjects;
 
     public FiltersViewModel(Navigator navigator, PersistenceManager persistenceManager, DevUtils.Logger logger) {
         super(navigator, persistenceManager, logger);
+
         filter = persistenceManager.loadFilter();
+        subjects = DevUtils.getFakeSubjects();
     }
 
     public String getFormattedDistance() {
         return filter.maxDistance + "m";
     }
 
-    public void onPersistClick(View view) {
+    public void save(View view) {
         persistenceManager.saveFilter(filter);
         persistenceManager.requestSync();
     }
