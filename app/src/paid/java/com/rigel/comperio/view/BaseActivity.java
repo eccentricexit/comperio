ddc15a80f65ea97b343f57370e7e2f7926370ab0
Matheus Faria de Alencar <mtsalenc@gmail.com>
2017-12-05 19:55:26 -0200

    fix: Paid version methods

--- app/src/paid/java/com/rigel/comperio/view/BaseActivity.java
+++ app/src/paid/java/com/rigel/comperio/view/BaseActivity.java
@@ -1,37 +1,33 @@
 package com.rigel.comperio.view;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.provider.BaseColumns;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.rigel.comperio.DevUtils;
-import com.rigel.comperio.Navigator;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.rigel.comperio.LoggingManager;
+import com.rigel.comperio.NavigationManager;
 import com.rigel.comperio.PersistenceManager;
 import com.rigel.comperio.R;
-import com.rigel.comperio.data.ComperioContract;
-import com.rigel.comperio.model.Filter;
-import com.rigel.comperio.model.Schedule;
-import com.rigel.comperio.sync.ComperioService;
-import com.rigel.comperio.sync.SyncAdapter;
+import com.rigel.comperio.model.UserData;
 
-import retrofit2.Callback;
-import timber.log.Timber;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 0;
+    private static final String LOG_TAG = BaseColumns.class.getSimpleName();
 
-    private Navigator navigator;
-    private PersistenceManager persistenceManager;
-    private DevUtils.Logger logger;
+    private static final int REQUEST_CODE_LOCATION = 1;
+
+    private NavigationManager navigationManager;
+    private PersistenceManager persistanceManagerImpl;
+    private LoggingManager logger;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,180 +39,105 @@ public abstract class BaseActivity extends AppCompatActivity {
 	    // No ads in paid version
     }
 
-    protected Navigator getNavigator() {
+    protected abstract void initDataBinding();
 
-        if (navigator != null) {
-            return navigator;
+    private void initLocationServices() {
+
+        if (ContextCompat.checkSelfPermission( this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION )
+                != PackageManager.PERMISSION_GRANTED ) {
+
+            return;
         }
 
-        navigator = new Navigator() {
-
-            @Override
-            public void navigateToHomeActivity() {
-                navigate(MainActivity.class);
-            }
-
-            @Override
-            public void navigateToDetailsActivity(Schedule schedule) {
-                Intent intent = new Intent(BaseActivity.this, ScheduleDetailActivity.class);
-                intent.putExtra(BaseActivity.this.getString(R.string.EXTRA_SCHEDULE), schedule);
-                startActivity(intent);
-            }
-
-            @Override
-            public void navigateToAddContact(Schedule schedule) {
-                Intent intent = new Intent(Intent.ACTION_INSERT);
-                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
-
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, schedule.teacherName);
-                intent.putExtra(ContactsContract.Intents.Insert.PHONE, schedule.teacherPhone);
-                intent.putExtra(ContactsContract.Intents.Insert.COMPANY, getString(R.string.app_name));
-                intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE,
-                        schedule.subjectName+" "+getString(R.string.teacher));
-
-                startActivityForResult(intent,REQUEST_CODE);
-            }
-
-            @Override
-            public void navigateToNewSchedule() {
-                navigate(NewScheduleActivity.class);
-            }
-
-
-            private void navigate(Class<?> destination) {
-                Intent intent = new Intent(BaseActivity.this, destination);
-                startActivity(intent);
-            }
-        };
-
-        return navigator;
-    }
-
-    protected PersistenceManager getPersistenceManager() {
-        if (persistenceManager != null) {
-            return persistenceManager;
+        if(alreadySavedLocation()){
+            return;
         }
 
-        persistenceManager = new PersistenceManager() {
+        FusedLocationProviderClient fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
 
-            ComperioService comperioService;
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                Float[] loc = new Float[2];
+                                if (location == null) {
+                                    loc[0] = 0f;
+                                    loc[1] = 0f;
+                                }else{
+                                    loc[0] = (float) location.getLongitude();
+                                    loc[1] = (float) location.getLatitude();
+                                }
 
-            @Override
-            public Filter loadFilter() {
-                Gson gson = new Gson();
-                String json = getComperioPreferences()
-                        .getString(getString(R.string.FILTER_KEY), "");
-
-                if (!json.equals("")) {
-                    return gson.fromJson(json, Filter.class);
-                } else {
-                    return new Filter();
-                }
-            }
-
-            @Override
-            public void saveFilter(Filter filter) {
-                String json = new Gson().toJson(filter);
-                getEditor().putString(getString(R.string.FILTER_KEY), json).commit();
-            }
-
-            @Override
-            public void addToFavorites(Schedule schedule) {
-                if(alreadyInFavorites(schedule)){
-                    return;
-                }
-
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(ComperioContract.FavoriteEntry.COLUMN_SCHEDULE_KEY, schedule._id);
-
-                getContentResolver()
-                        .insert(ComperioContract.FavoriteEntry.CONTENT_URI, contentValues);
-            }
-
-            @Override
-            public void removeFromFavorites(Schedule schedule) {
-                getContentResolver().delete(
-                        ComperioContract.FavoriteEntry.CONTENT_URI,
-                        ComperioContract.FavoriteEntry.COLUMN_SCHEDULE_KEY + " = ?",
-                        new String[]{schedule._id});
-            }
-
-            @Override
-            public void requestSync() {
-                SyncAdapter.syncImmediately(BaseActivity.this);
-            }
-
-            @Override
-            public void publishNewSchedule(Schedule schedule, Callback<JsonElement> callback) {
-
-                getComperioService().publishNewSchedule(schedule.teacherName,
-                        DevUtils.getFakeUrl(),
-                        DevUtils.getFakeRating(),
-                        schedule.teacherPhone,
-                        schedule.subjectName,
-                        schedule.loc,
-                        schedule.hourPrice,
-                        schedule.teacherStory,
-                        callback);
-
-            }
-
-            private ComperioService getComperioService(){
-                if(comperioService==null){
-                    comperioService =
-                            ComperioApplication.get(BaseActivity.this).getComperioService();
-                }
-
-                return  comperioService;
-            }
-
-            private boolean alreadyInFavorites(Schedule schedule) {
-                Cursor cursor = getContentResolver().query(
-                        ComperioContract.FavoriteEntry.CONTENT_URI,
-                        null,
-                        ComperioContract.FavoriteEntry.COLUMN_SCHEDULE_KEY,
-                        new String[]{schedule._id},
-                        null
+                                UserData userData = getPersistanceManager().loadUserData();
+                                userData.userLoc = loc;
+                                persistanceManagerImpl.saveUserData(userData);
+                            }
+                        }
                 );
-                int count = cursor.getCount();
-                cursor.close();
-
-                return count>0;
-            }
-
-            private SharedPreferences.Editor getEditor() {
-                SharedPreferences sharedPref = getComperioPreferences();
-                return sharedPref.edit();
-            }
-
-            private SharedPreferences getComperioPreferences() {
-                String key = getString(R.string.SHARED_PREFERENCES_KEY);
-                return getSharedPreferences(key, 0);
-            }
-        };
-
-        return persistenceManager;
     }
 
-    protected DevUtils.Logger getLogger() {
-        if (logger != null) {
-            return logger;
+    private void checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_LOCATION);
+        } else {
+            initLocationServices();
+        }
+    }
+
+    private boolean alreadySavedLocation() {
+        UserData userData = getPersistanceManager().loadUserData();
+        return userData != null &&
+                userData.userLoc[0]!=null &&
+                userData.userLoc[1]!=null &&
+                userData.userLoc[0]!=0f &&
+                userData.userLoc[1]!=0f;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if(requestCode!=REQUEST_CODE_LOCATION){
+            return;
         }
 
-        logger = new DevUtils.Logger() {
-            @Override
-            public void log(String logMessage) {
-                Timber.d(logMessage);
-            }
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initLocationServices();
+        } else {
+            logger.toast(getString(R.string.msg_location_disabled));
+        }
+    }
 
-            @Override
-            public void toast(String message) {
-                Toast.makeText(BaseActivity.this, message, Toast.LENGTH_SHORT).show();
-            }
-        };
+    protected NavigationManager getNavigationManager() {
+        if (navigationManager == null) {
+            navigationManager = new NavigationManager(this);
+        }
+
+        return navigationManager;
+    }
+
+    protected PersistenceManager getPersistanceManager() {
+        if (persistanceManagerImpl == null) {
+            persistanceManagerImpl = new PersistenceManager(this);
+        }
+
+        return persistanceManagerImpl;
+    }
+
+    protected LoggingManager getLogger() {
+        if (logger == null) {
+            logger = new LoggingManager(this,LOG_TAG);
+        }
 
         return logger;
     }
 
-    protected abstract void initDataBinding();
+
 }
